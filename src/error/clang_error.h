d25c4a4442d131860bb4dfd170592aca2d6b0716
a1exxd0 <alexxdo2004@gmail.com>
2025-11-13 15:19:24 +0000

    chore: move colors for terminal stuff in unicode into visible namespace

--- src/error/clang_error.h
+++ src/error/clang_error.h
@@ -8,6 +8,16 @@
 
 namespace mccomp {
 
+namespace text_colors {
+constexpr const char *BOLD = "\033[1m";
+constexpr const char *RED = "\033[31m";
+constexpr const char *YELLOW = "\033[33m";
+constexpr const char *RESET = "\033[0m";
+constexpr const char *GREEN = "\033[32;1m";
+constexpr const char *BLUE = "\033[34;1m";
+constexpr const char *CYAN = "\033[1;96m";
+}; // namespace text_colors
+
 enum class ClangErrorSeverity {
   NOTE,
   WARNING,
