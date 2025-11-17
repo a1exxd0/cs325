86bfeecddb4ce39a1bd34c686d451f341de3e33c
a1exxd0 <alexxdo2004@gmail.com>
2025-11-17 17:49:11 +0000

    fix(ast): missing pragma onces, bad toplevel incl

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -2,9 +2,11 @@
 
 #define ALLOWED_INCLUDE
 #include <ast/_internal/ast_node.h>
+#include <ast/_internal/ctx.h>
 #include <ast/_internal/decl_node.h>
 #include <ast/_internal/expr_node.h>
 #include <ast/_internal/stmt_node.h>
 #include <ast/_internal/translation_unit.h>
+#include <ast/_internal/type.h>
 #include <ast/_internal/visitor.h>
 #undef ALLOWED_INCLUDE
